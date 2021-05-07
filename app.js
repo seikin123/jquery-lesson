6d24f057441869f88682ad445565ed4fd9dc8ea8
ヨシダタイキ <abc521fjr7@gmail.com>
2021-05-07 16:07:45 +0900

    マウスクリック

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+// $('.セレクタ名').on('click', function() {
+//   イベント発生時に行われる処理
+// });
+
 $(function(){
-  $('.box1').mouseover(function(){
+  $('.box1').on('click',function(){
     $('.box1').addClass('box1-ext');
   });
   $('.box1').mouseout(function(){
