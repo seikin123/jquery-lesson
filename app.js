c6bd9d27db221aff854047afcc0ab20001a74596
ヨシダタイキ <abc521fjr7@gmail.com>
2021-05-07 16:14:45 +0900

    thisを使ったイベントの発生

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-// $('.セレクタ名').on('click', function() {
-//   イベント発生時に行われる処理
-// });
-
 $(function(){
   $('.box1').on('click',function(){
-    $('.box1').addClass('box1-ext');
-  });
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box1-ext');
+    $(this).slideUp();
   });
 });
